83624abb66922f89f1b99a78986989feac7eb06d
jmn-exe <amouxmerle@gmail.com>
2023-09-20 16:26:38 +0800

    changes for v2

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
  
 function App() { 
   return ( 
-    <div className=\App\> 
-      <header className=\App-header\> 
-        <img src={logo} className=\App-logo\ alt=\logo\ /> 
+    <div className="App"> 
+      <header className="App-header"> 
+        <img src={logo} className="App-logo" alt="logo" /> 
         <h1>Hello from V2</h1> 
       </header> 
     </div> 
